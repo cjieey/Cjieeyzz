14f87e8c2f40bfba0ff206cc8e1aa90343b86bc8
C3jieey <105692921+cjieey@users.noreply.github.com>
2024-03-23 08:35:25 +0800

    Add files via upload

---
+++ js/addbtn.js
@@ -0,0 +1,57 @@
+const addProjectBtn = document.getElementById('addProjectBtn');
+const popupForm = document.getElementById('popupForm');
+const uploadForm = document.getElementById('uploadForm');
+const collect = document.getElementById("collect");
+const fileInput = document.getElementById('fileInput');
+const linkInput = document.getElementById('linkInput');
+const titleInput = document.getElementById('titleInput');
+const descriptionInput = document.getElementById('descriptionInput');
+const cancel = document.getElementById('cancel');
+
+addProjectBtn.addEventListener('click', () => {
+    popupForm.style.display = 'block';
+    const projectCollections = document.querySelectorAll('.project_collection');
+    
+    projectCollections.forEach(project => {
+        project.style.pointerEvents = 'none';
+    });
+    cancel.addEventListener('click', () => {
+        popupForm.style.display = 'none';
+        const projectCollections = document.querySelectorAll('.project_collection');
+        
+        projectCollections.forEach(project => {
+            project.style.pointerEvents = 'auto';
+        });
+    });
+});
+
+
+
+
+uploadForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(fileInput.files[0]);
+    img.alt = titleInput.value;
+    img.className = 'pimg';
+
+    const projectHTML = `
+        <div class="project_collection">
+            <img class="rgb" src="icon/iconrgb.png" alt="">
+            <a target="_blank" href="${linkInput.value}">
+                ${img.outerHTML}
+            </a>
+            <h3>${titleInput.value}</h3>
+            <div class="desc">${descriptionInput.value}</div>
+            <div class="label">
+                <div class="lbl">HTML</div>
+                <div class="lbl">CSS</div>
+                <div class="lbl">JS</div>
+            </div>
+        </div>`;
+
+    collect.innerHTML += projectHTML;
+    popupForm.style.display = 'none';
+    uploadForm.reset();
+});
